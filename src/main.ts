79bba9f0fd40943795cabd3a5e704fa9101c9637
Ninhache <mr.carambar27@gmail.com>
2024-12-03 01:45:53 +0100

    feat: having funz

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-type StandardHtmlTags = "h1" | "div" | "span";
+type StandardHtmlTags =
+  | "h1"
+  | "div"
+  | "span"
+  | "header"
+  | "main"
+  | "ul"
+  | "li"
+  | "p";
 type VoidHtmlTags = "img";
 
 type HtmlTags = StandardHtmlTags | VoidHtmlTags;
@@ -34,13 +42,17 @@ type HtmlElements<T extends HtmlTags> = T extends StandardHtmlTags
       ? ""
       : ` ${AttributeString<AttributesFor<T>>}`} />`;
 
+type HtmlContent<T extends HtmlTags> = T extends StandardHtmlTags
+  ? HtmlElements<HtmlTags> | Array<string | HtmlElements<HtmlTags>> | string // < I don't like the usage of raw string here but.. i'm a bit forced with that type structure
+  : never;
+
 function renderElement<T extends HtmlTags>(
   tag: T,
   attributes: T extends keyof SpecificAttributes
     ? Pick<AttributesFor<T>, keyof SpecificAttributes[T]> &
         Partial<GlobalAttributes>
-    : Partial<AttributesFor<T>>,
-  content?: T extends StandardHtmlTags ? string : never
+    : Partial<AttributesFor<T>> | null,
+  content?: HtmlContent<T>
 ): HtmlElements<T> {
   const attrString = Object.entries(attributes || {})
     .map(([key, value]) => `${key}="${value}"`)
@@ -51,11 +63,20 @@ function renderElement<T extends HtmlTags>(
   return `<${tag} ${attrString} />` as HtmlElements<T>;
 }
 
-const title = renderElement("img", {
-  style: "color: red",
-  src: "https://picsum.photos/200",
-  alt: "Random unsplash image",
-  width: 200,
-  height: 200,
-});
-document.querySelector(".container > header")!.innerHTML = title;
+const fullHtml = renderElement("div", { id: "app" }, [
+  renderElement("header", {}, [
+    renderElement("h1", { style: "color: blue" }, "Welcome to My App"),
+    renderElement("img", {
+      src: "https://picsum.photos/300",
+      alt: "A random image",
+    }),
+  ]),
+  renderElement("main", { class: "content" }, [
+    renderElement("p", null, "This is a paragraph with some text."),
+    renderElement("ul", null, [
+      renderElement("li", null, "First item"),
+      renderElement("li", null, "Second item"),
+    ]),
+  ]),
+]);
+document.querySelector(".container")!.innerHTML = fullHtml;
